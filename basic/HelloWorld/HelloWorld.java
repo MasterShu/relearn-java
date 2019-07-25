17463365b4fc0024b3b0d1e94577247325e72c4d
MasterShu <1562688988@qq.com>
2019-07-25 17:43:52 +0800

    Created HelloWorld.java

---
+++ basic/HelloWorld/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * HelloWorld
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        你好java(); // Not recommend!
+        spaceLine();
+        System.out.println("Hello Java");
+    }
+
+    // use Chinese is not recommend.
+    public static void 你好java () {
+        System.out.println("你好 Java");
+    }
+
+    public static void spaceLine() {
+        System.out.println();
+    }
+}
